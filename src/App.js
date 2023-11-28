86e67e86390f5f754f8539bbdb2a3fa938d20635
Ajeet Gupta <ajeetskb@gmail.com>
2023-11-28 21:45:17 +0530

    bottom-three-sections

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import ClientView from './components/ClientViews/ClientView';
+import WhyKodeHash from './components/WhyKodeHash/WhyKodeHash';
 
 function App() {
   return (
     <div className="App">
+      <WhyKodeHash/>
       <ClientView/>
       <Footer/>
     </div>
---
+++ src/components/WhyKodeHash/WhyKodeHash.jsx
@@ -0,0 +1,173 @@
+import React from 'react'
+import hash from '../../assets/images/ggg.png'
+import './WhyKodeHash.css'
+
+function WhyKodeHash() {
+  return (
+      <>
+          <div className="backk" style={{ width: "100%", height: "40rem" }}>
+              <div
+                  style={{ width: "100%", textAlign: "center", color: "white" }}
+              >
+                  <h1 style={{ paddingTop: "35px" }}>Why KodeHash?</h1>
+              </div>
+              <div
+                  style={{
+                      width: "100%",
+                      textAlign: "center",
+                      color: "white",
+                      marginTop: "25px",
+                  }}
+              >
+                  <h5 style={{ fontWeight: "lighter" }}>
+                      <span style={{ display: "inline-block" }}>
+                          {" "}
+                          Discover why multinational corporations favour
+                          Kodehash for fulfilling their software development
+                          requirements
+                      </span>
+                      <br></br>
+                      <span>through remote developer recruitment:</span>
+                  </h5>
+              </div>
+              <div
+                  style={{
+                      width: "100%",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: "15px",
+                      marginTop: "30px",
+                  }}
+              >
+                  <div
+                      style={{
+                          width: "18%",
+                          height: "250px",
+                          backgroundColor: "grey",
+                      }}
+                  >
+                      <div
+                          style={{
+                              display: "flex",
+                              flexDirection: "column",
+                              alignItems: "center",
+                              justifyContent: "space-evenly",
+                              height: "80%",
+                          }}
+                      >
+                          <h4 style={{ textAlign: "center", color: "white" }}>
+                              Extensive Global
+                              <br /> Network
+                          </h4>
+                          <hr
+                              style={{ width: "40px", marginTop: "-15px" }}
+                          ></hr>
+                          <p style={{ textAlign: "center", color: "white" }}>
+                              Kodehash offers access to a vast pool of remote
+                              developers from around the world, ensuring diverse
+                              talent options.
+                          </p>
+                      </div>
+                  </div>
+                  <div
+                      style={{
+                          width: "18%",
+                          height: "250px",
+                          backgroundColor: "grey",
+                          position: "relative",
+                          top: "30px",
+                      }}
+                  >
+                      <div
+                          style={{
+                              display: "flex",
+                              flexDirection: "column",
+                              alignItems: "center",
+                              justifyContent: "space-evenly",
+                              height: "80%",
+                          }}
+                      >
+                          <h4 style={{ textAlign: "center", color: "white" }}>
+                              Extensive Global
+                              <br /> Network
+                          </h4>
+                          <hr
+                              style={{ width: "40px", marginTop: "-15px" }}
+                          ></hr>
+                          <p style={{ textAlign: "center", color: "white" }}>
+                              Kodehash offers access to a vast pool of remote
+                              developers from around the world, ensuring diverse
+                              talent options.
+                          </p>
+                      </div>
+                  </div>
+                  <div
+                      style={{
+                          width: "18%",
+                          height: "250px",
+                          backgroundColor: "grey",
+                      }}
+                  >
+                      <div
+                          style={{
+                              display: "flex",
+                              flexDirection: "column",
+                              alignItems: "center",
+                              justifyContent: "space-evenly",
+                              height: "80%",
+                          }}
+                      >
+                          <h4 style={{ textAlign: "center", color: "white" }}>
+                              Extensive Global
+                              <br /> Network
+                          </h4>
+                          <hr
+                              style={{ width: "40px", marginTop: "-15px" }}
+                          ></hr>
+                          <p style={{ textAlign: "center", color: "white" }}>
+                              Kodehash offers access to a vast pool of remote
+                              developers from around the world, ensuring diverse
+                              talent options.
+                          </p>
+                      </div>
+                  </div>
+                  <div
+                      style={{
+                          width: "18%",
+                          height: "250px",
+                          backgroundColor: "grey",
+                          position: "relative",
+                          top: "30px",
+                      }}
+                  >
+                      <div
+                          style={{
+                              display: "flex",
+                              flexDirection: "column",
+                              alignItems: "center",
+                              justifyContent: "space-evenly",
+                              height: "80%",
+                          }}
+                      >
+                          <h4 style={{ textAlign: "center", color: "white" }}>
+                              Extensive Global
+                              <br /> Network
+                          </h4>
+                          <hr
+                              style={{ width: "40px", marginTop: "-15px" }}
+                          ></hr>
+                          <p style={{ textAlign: "center", color: "white" }}>
+                              Kodehash offers access to a vast pool of remote
+                              developers from around the world, ensuring diverse
+                              talent options.
+                          </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </>
+  );
+}
+
+export default WhyKodeHash;
